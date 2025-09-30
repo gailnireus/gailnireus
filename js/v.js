8a113b0ec2d2daf840151c09bc3fe1cbcf72f87a
gailnireus <gailnireus@yahoo.com>
2025-09-30 08:57:39 +0800

    Delete js directory

--- js/v.js
+++
@@ -1,7 +0,0 @@
-function showPageV() {
-  const content = document.getElementById("content");
-  content.innerHTML = `
-    <h1>Page V</h1>
-    <p>Ini halaman V.</p>
-  `;
-}
--- js/x.js
+++
@@ -1,7 +0,0 @@
-function showPageX() {
-  const content = document.getElementById("content");
-  content.innerHTML = `
-    <h1>Page X</h1>
-    <p>Ini halaman X.</p>
-  `;
-}
